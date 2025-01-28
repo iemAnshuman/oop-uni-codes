b5cbb6c9215ce23715d0f1fe1437208069bcfaf1
iemAnshuman <asquare567@gmail.com>
2025-01-29 01:49:16 +0530

    changed exp2 to not use scanner

--- exp2.java
+++ exp2.java
@@ -1,32 +1,20 @@
-import java.util.Scanner;
+class Exp2 {
+    public static void main(String args[]) {
+        // Predefined variables
+        String name = "Anshuman Agrawal";
+        String state = "Sikkim";
+        String city = "Gangtok";
+        String locality = "Near Mg road";
+        int a = 10, b = 20, c = 30;
 
-class Exp2{
-  public static void main(String args[]){
-    Scanner scn = new Scanner(System.in);
+        // Output statements remain the same, but now use predefined variables
+        System.out.println("Hello, " + name);
 
-    System.out.println("Enter your name: ");
-    String name = scn.nextLine();
+        System.out.println("ADDRESS:-");
 
-    System.out.println("Hello, " + name);
+        System.out.println("Nice to know, that you are from " + state + "\nAnd your city is:" + city + "\nlocality:" + locality);
 
-    System.out.println("ADDRESS:-");
-    System.out.println("Enter State: ");
-    String state = scn.nextLine();
+        System.out.println("Birthday: 07\\12\\2004");
 
-    System.out.println("Enter city: ");
-    String city = scn.nextLine();
-
-    System.out.println("Locality: ");
-    String locality = scn.nextLine();
-
-    System.out.println("Nice to know, that you are from " + state + "\nAnd your city is:\t" + city + "\nlocality:\t" + locality);
-
-    System.out.println("Birthday: 07\\12\\2004");
-
-    System.out.println("\\n");
-    System.out.println("\\\\");
-    int a = 10, b = 20, c = 30;
-    System.out.println("values are: " + a + ", " + b + ", " + c);
-    System.out.println("values are added now: " + (a + b) + " and " + c);
-  }
+    }
 }
